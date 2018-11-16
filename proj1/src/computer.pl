7652c9ae566600f358d92ee401a95c51074ee5a7
ff7 <francisco.tf4@gmail.com>
2018-11-16 10:07:40 +0000

    Working on alomost defeat save

--- proj1/src/computer.pl
+++ proj1/src/computer.pl
@@ -13,17 +13,52 @@ choose_move(Player, Board, 2, OldRow, OldColumn, NewRow, NewColumn) :-
     handleFlagAlmostVictory(Flag, Player, Board, OldRow, OldColumn, NewRow, NewColumn, OldRowTemp, OldColumnTemp, NewRowTemp, NewColumnTemp).
 
 handleFlagAlmostVictory(2, Player, Board, OldRow, OldColumn, NewRow, NewColumn, OldRowTemp, OldColumnTemp, NewRowTemp, NewColumnTemp) :-
-    OldRow = OldRowTemp, 
-    OldColumn = OldColumnTemp, 
-    NewRow = NewRowTemp, 
+    OldRow = OldRowTemp,
+    OldColumn = OldColumnTemp,
+    NewRow = NewRowTemp,
     NewColumn = NewColumnTemp.
 
 handleFlagAlmostVictory(_, Player, Board, OldRow, OldColumn, NewRow, NewColumn, OldRowTemp, OldColumnTemp, NewRowTemp, NewColumnTemp) :-
     checkIfAlmostDefeat(Player, Board, OldRowTemp, OldColumnTemp, NewRowTemp, NewColumnTemp, Flag),
+    handleFlagAlmostDefeat(Flag, Player, Board, OldRow, OldColumn, NewRow, NewColumn, OldRowTemp, OldColumnTemp, NewRowTemp, NewColumnTemp).
     write('handle').
 
+% ------------------------------------------------------------------------------------------------------
+
+handleFlagAlmostDefeat(2, Player, Board, OldRow, OldColumn, NewRow, NewColumn, OldRowTemp, OldColumnTemp, NewRowTemp, NewColumnTemp) :-
+    write('Almost Defeat'), nl,
+    possibleDefense(isDefensible, Player, Board, OldRowTemp, OldColumnTemp, NewRowTemp, NewColumnTemp),
+    generateMove(isDefensible, Player, Board, OldRow, OldColumn, NewRow, NewColumn, OldRowTemp, OldColumnTemp, NewRowTemp, NewColumnTemp).
+
+possibleDefense(isDefensible, Player, Board, OldRowTemp, OldColumnTemp, NewRowTemp, NewColumnTemp) :-
+    valid_moves(Player, Board, [], Ret),
+    % Iterar e ver se pode movimentar-se para a jogada que salva o jogo
+    % member(X, [One]).
+
+generateMove(1, Player, Board, OldRow, OldColumn, NewRow, NewColumn, OldRowTemp, OldColumnTemp, NewRowTemp, NewColumnTemp):-
+    valid_moves(Player, Board, [], Ret),
+    random_member([OldRow, OldColumn, NewRow, NewColumn], Ret).
+
+generateMove(2, Player, Board, OldRow, OldColumn, NewRow, NewColumn, OldRowTemp, OldColumnTemp, NewRowTemp, NewColumnTemp):-
+    OldRow = OldRowTemp,
+    OldColumn = OldColumnTemp,
+    NewRow = NewRowTemp,
+    NewColumn = NewColumnTemp.
 
 
+OtherPlayer('Player1', P2) :-
+  P2 = 'Player2'.
+
+OtherPlayer('Player2', P2) :-
+  P2 = 'Player1'.
+
+checkIfAlmostDefeat(Player, Board, OldRowTemp, OldColumnTemp, NewRowTemp, NewColumnTemp, Flag) :-
+  OtherPlayer(Player, P2)
+  valid_moves(P2, Board, [], Ret),
+  checkEachMove(P2, Board, Ret, Flag2, OR, OC, NR, NC),
+  handleFlag(Flag2, Flag, OR, OC, NR, NC, OldRow, OldColumn, NewRow, NewColumn).
+
+% ------------------------------------------------------------------------------------------------------
 
 checkIfAlmostVictory(Player, Board, OldRow, OldColumn, NewRow, NewColumn, Flag) :-
     valid_moves(Player, Board, [], Ret),
@@ -31,9 +66,9 @@ checkIfAlmostVictory(Player, Board, OldRow, OldColumn, NewRow, NewColumn, Flag) 
     handleFlag(Flag2, Flag, OR, OC, NR, NC, OldRow, OldColumn, NewRow, NewColumn).
 
 handleFlag(2, Flag, OR, OC, NR, NC, OldRow, OldColumn, NewRow, NewColumn) :-
-    OldRow = OR, 
-    OldColumn = OC, 
-    NewRow = NR, 
+    OldRow = OR,
+    OldColumn = OC,
+    NewRow = NR,
     NewColumn = NC,
     Flag is 2.
 
@@ -49,9 +84,9 @@ checkEachMove(Player, Board, [[OR,OC,NR,NC]|T], Flag2, OldRow, OldColumn, NewRow
     handleCheckVictory(Player, Board, T, OR, OC, NR, NC, Result, Flag2, OldRow, OldColumn, NewRow, NewColumn).
 
 handleCheckVictory(Player, Board, T, OR, OC, NR, NC, 2, Flag2, OldRow, OldColumn, NewRow, NewColumn) :-
-    OldRow = OR, 
-    OldColumn = OC, 
-    NewRow = NR, 
+    OldRow = OR,
+    OldColumn = OC,
+    NewRow = NR,
     NewColumn = NC,
     Flag2 is 2.
 
